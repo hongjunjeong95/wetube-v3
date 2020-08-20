276241753a3c7c3455332f338f2eb0080b7a0f02
zpskek <wjdghdwns@gmail.com>
2020-08-20 11:40:27 +0900

    Install various middlewares

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(helmet());
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
