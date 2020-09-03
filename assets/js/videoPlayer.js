06376cf02babde52e38bdcf3add45a3214e65493
zpskek <wjdghdwns0@gmail.com>
2020-09-03 14:09:02 +0900

    VolumeRange on videoPlayer

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById('jsVideoPlayer');
 const videoPlayer = document.querySelector('#jsVideoPlayer video');
 const playBtn = document.getElementById('jsPlayBtn');
 const volumeBtn = document.getElementById('jsVolumeBtn');
+const volumeRange = document.getElementById('jsVolumeRange');
 
 const handlePlayClick = () => {
   if (videoPlayer.paused) {
@@ -17,8 +18,26 @@ const handleVolumeClick = () => {
   if (videoPlayer.muted) {
     videoPlayer.muted = false;
     volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    volumeRange.value = videoPlayer.volume;
   } else {
     videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    volumeRange.value = 0;
+  }
+};
+
+const handleVolumeRange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  videoPlayer.volume = value;
+  if (value >= 0.7) {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else if (value >= 0.4) {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+  } else if (value >= 0.1) {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+  } else {
     volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 };
@@ -27,6 +46,7 @@ const init = () => {
   playBtn.addEventListener('click', handlePlayClick);
   videoPlayer.addEventListener('click', handlePlayClick);
   volumeBtn.addEventListener('click', handleVolumeClick);
+  volumeRange.addEventListener('input', handleVolumeRange);
 };
 
 if (videoContainer) {
