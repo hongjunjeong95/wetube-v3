659bb54f7b5bb3576343ac448757e31c1600f016
zpskek <wjdghdwns0@gmail.com>
2020-09-03 13:54:29 +0900

    VolumeBtn on videoPlayer

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,6 +1,7 @@
 const videoContainer = document.getElementById('jsVideoPlayer');
 const videoPlayer = document.querySelector('#jsVideoPlayer video');
 const playBtn = document.getElementById('jsPlayBtn');
+const volumeBtn = document.getElementById('jsVolumeBtn');
 
 const handlePlayClick = () => {
   if (videoPlayer.paused) {
@@ -12,9 +13,20 @@ const handlePlayClick = () => {
   }
 };
 
+const handleVolumeClick = () => {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+};
+
 const init = () => {
   playBtn.addEventListener('click', handlePlayClick);
   videoPlayer.addEventListener('click', handlePlayClick);
+  volumeBtn.addEventListener('click', handleVolumeClick);
 };
 
 if (videoContainer) {
